6c035c73fd2e3bef7367ef9814cc0aeb4d034546
zxGit <1205188085@qq.com>
2019-01-21 14:22:14 +0800

    idea第2次提交

--- src/com/itmayiedu/GitTest02.java
+++ src/com/itmayiedu/GitTest02.java
@@ -2,7 +2,8 @@ package com.itmayiedu;
 
 public class GitTest02 {
     public static void main(String[] args) {
-        System.out.println("创建dev002分支");
-        System.out.println("0002");
+        System.out.println("0001");
+        System.out.println("创建dev001分支");
+        System.out.println("0001");
     }
 }
